4adde69e5a54e6ef21a711080682142e0921f098
Dj <148027370+djsy104@users.noreply.github.com>
2025-09-19 15:10:08 -0700

    feat: setup grid layout and prettier formatting

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const container = document.querySelector("#container");
+const container = document.querySelector('#container');
 const fragment = document.createDocumentFragment(); //Temporary staging area before adding to the DOM
 createGrid(256);
 
 function createGrid(count) {
-    container.innerHTML = "" // Clears container before re-creating
-    for (let i = 0; i < count; i++) {
-        const div = document.createElement("div")
-        div.className = "gridItem"
-        fragment.appendChild(div)
-    }
+  container.innerHTML = ''; // Clears container before re-creating
+  for (let i = 0; i < count; i++) {
+    const div = document.createElement('div');
+    div.className = 'gridItem';
+    fragment.appendChild(div);
+  }
 
-    container.appendChild(fragment);
+  container.appendChild(fragment);
 }

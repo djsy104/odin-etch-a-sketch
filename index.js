7261ac5b689de035355df45f9124fcd12fad4c9a
djsy104 <148027370+djsy104@users.noreply.github.com>
2025-09-25 16:13:12 -0700

    feat: completed etch-a-sketch functionality
    - Keeps container same size, regardless of grid size
    - Implemented button prompt that asks user for grid size
    - Recreates the grid after prompt

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const container = document.querySelector('#container');
 const fragment = document.createDocumentFragment(); //Temporary staging area before adding to the DOM
 const button = document.querySelector('#pop-up');
-createGrid(256);
+let gridSize = getComputedStyle(document.documentElement).getPropertyValue("--n"); 
+//document.documentElement grabs the "html" doc
+//getComputerdStyle() grabs the style after all CSS is applied
+
+createGrid(gridSize);
 
 function createGrid(count) {
+  count = count**2
   container.innerHTML = ''; // Clears container before re-creating
   for (let i = 0; i < count; i++) {
     const div = document.createElement('div');
@@ -14,4 +19,13 @@ function createGrid(count) {
   container.appendChild(fragment);
 }
 
-button.addEventListener('click', () => {});
+button.addEventListener('click', () => {
+  gridSize = prompt("Please enter the grid size.");
+
+  if (gridSize > 100) {
+    alert("Please enter a value between 1 - 100");
+  } else {
+    document.documentElement.style.setProperty("--n", gridSize);
+    createGrid(gridSize);
+  }
+});
